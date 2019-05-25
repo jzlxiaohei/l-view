5dee4b1918dbb96ec22a0e1d55b45e692fa8c283
zilong <leonard.jiang@liulishuo.com>
2019-05-25 15:24:40 +0800

    feat(button): add text editor

--- src/widgets/button/edit.jsx
+++ src/widgets/button/edit.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { observer } from 'mobx-react';
 import PropEditor from 'comps/prop-editor';
 import ButtonModel from './model';
+import { FormInput } from 'comps/form-item';
 
 
 @observer
@@ -11,11 +12,23 @@ class ButtonEdit extends React.Component {
     model: PropTypes.instanceOf(ButtonModel),
   };
 
+  renderButtonEditor(model) {
+    return (
+      <React.Fragment>
+        <FormInput label="text" model={model} path="attr.text" />
+      </React.Fragment>
+    )
+  }
+
+
   render() {
     return (
-      <div className="widget-edit-image">
+      <div className="widget-edit-button">
         <PropEditor
           model={this.props.model}
+          renderAfterBasicInfo={model => {
+            return this.renderButtonEditor(model);
+          }}
         />
       </div>
     );
